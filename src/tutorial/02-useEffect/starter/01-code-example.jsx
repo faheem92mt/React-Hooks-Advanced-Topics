29242ad740d69c7103a00b3cabb349ded2083a9f
faheem92mt <faheemibnhabib@gmail.com>
2024-07-30 22:21:15 +0800

    comment

--- src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -2,7 +2,18 @@ import { useState } from 'react'
 
 const CodeExample = () => {
   const [value, setValue] = useState(0)
-  //git
+
+  const sayHello = () => {
+    console.log('Hello')
+  }
+
+  // the sayHello() function gets executed everytime setValue() is invoked
+  // this happense because this function will get executed in each re-render
+
+  // which can potentially cause an infinite loop if this function will have
+  // an invocation of setValue() inside it
+  sayHello()
+
   return (
     <div>
       <h1>value : {value}</h1>
--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,41 @@
-const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users'
+import { useState, useEffect } from 'react'
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
-};
-export default FetchData;
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const getPeople = async () => {
+      try {
+        const resp = await fetch(url)
+        const data = await resp.json()
+        setUsers(data)
+        // console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getPeople()
+  }, [])
+
+  return (
+    <section>
+      <h3>github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h5>{login}</h5>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </section>
+  )
+}
+export default FetchData
